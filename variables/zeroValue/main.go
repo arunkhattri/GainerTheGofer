b6628b79052b6b5224755346856c2978046d1c6b
arunkhattri <arunkhattri@outlook.com>
2018-03-18 21:04:13 +0530

    added declaring variables

---
+++ variables/zeroValue/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// variables with value as 0
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Printf("a: %v \n", a)
+	fmt.Printf("b: %v \n", b)
+	fmt.Printf("c: %v \n", c)
+	fmt.Printf("d: %v \n", d)
+}
